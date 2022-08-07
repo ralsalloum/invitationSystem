e29ba68a1e824d93c67cfa0638028f0772669322
ralsalloum <ramisalloum12@hotmail.com>
2022-08-08 01:25:40 +0300

    mongodb-odm-bundle was installed

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
